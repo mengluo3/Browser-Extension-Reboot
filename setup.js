58a7838309d8f71491a03ec1b686e6c9b5c1a818
Meng Luo <mengmingluo1@gmail.com>
2020-09-20 16:19:53 -0700

    Added style.css for non-goal elements, which are weird because they are types/classes. CAN BE USED AS MVP.

--- setup.js
+++ setup.js
@@ -67,25 +67,6 @@ function addStickyNote(){
 			//Create a new div element that is a white rectangle. This is the background of the sticky note.
 		  	var box = document.createElement("div");
 			  	box.id="sticky";
-				box.style.width = boxWidth + "px";
-				box.style.minHeight = boxHeight + "px !important";
-				box.style.maxHeight = "600px";
-				box.style.background = "white";
-				box.style.borderRadius = "15px";
-				box.style.boxShadow = "1px 1px 2px 2px #D0D0D0";
-				box.style.color = "black";
-				box.style.position = "fixed";
-				box.style.right = "0";
-				box.style.bottom = "0";
-				box.style.margin = "1%";
-				box.style.zIndex="1000000000";
-				box.style.display = "block";
-				box.style.fontFamily = "Roboto, Calibri!important";
-				box.style.fontSize = "10pt";
-				box.style.textAlign = "left";
-				box.style.borderColor  = "#D0D0D0 !important";
-				box.style.borderStyle  = "solid !important";
-				box.style.borderWidth  = "1px !important";
 
 				//add new element to the clear background
 				document.body.append(box);
@@ -114,21 +95,7 @@ function addStickyNote(){
 			//Create a new button element. This is the '-' minimize button on the sticky note.
 			var min = document.createElement('button');
 				min.innerHTML ="—";
-
-				min.style.fontFamily="Roboto, Calibri";
-				min.style.textAlign = "center"; 
-				min.style.padding = "1%"; 
-				min.style.textDecoration = "none";
 				min.id="min";
-				//circ.style.width = "20px";
-				//circ.style.height = "20px";
-			 	min.style.borderRadius = "50%";
-			 	min.style.borderWidth = "0px";
-				min.style.color = "gray";
-				min.style.background = "white";
-				min.style.position = "absolute";
-				min.style.right = "15%";
-			    min.style.top = "20px";
 		  
 			 	document.getElementById("sticky").append(min); 
 			 	min.addEventListener ("click", function() {
@@ -139,20 +106,7 @@ function addStickyNote(){
 			//Create a new button element. This is the 'x' exit button on the sticky note.
 			var circ = document.createElement('button');
 				circ.innerHTML ="x";
-
-				circ.style.fontFamily="Roboto, Calibri";
-				circ.style.textAlign = "center"; 
-				circ.style.textDecoration = "none";
 				circ.id="min";
-				//circ.style.width = "20px";
-				//circ.style.height = "20px";
-			 	circ.style.borderRadius = "50%";
-			 	circ.style.borderWidth = "0px";
-				circ.style.color = "gray";
-				circ.style.background = "white";
-				circ.style.position = "absolute";
-				circ.style.right = "5%";
-			    circ.style.top = "20px";
 		  
 			 	document.getElementById("sticky").append(circ); 
 			 	circ.addEventListener ("click", function() {
@@ -162,23 +116,7 @@ function addStickyNote(){
 			 //Create a new button element. This is the button for adding a new goal.
 			 var plus = document.createElement('div');
 			 	plus.innerHTML ="+";
-
-			 	plus.style.fontFamily="Roboto, Calibri";
-			 	plus.style.textAlign = "center"; 
-			 	plus.style.textDecoration = "none";
 			 	plus.id="addGoalButton";
-			 	plus.style.width = "20px";
-			 	plus.style.height = "20px";
-			 	plus.style.borderWidth = "1px";
-			 	plus.style.borderStyle = "solid";
-			 	plus.style.borderColor="gray";
-			 	plus.style.borderRadius="10%";
-			 	plus.style.background = "white";
-			 	plus.style.color = "gray";
-			 	plus.style.fontWeight="700";
-			 	plus.style.position = "absolute";
-			 	plus.style.right = "5%";
-			    plus.style.bottom = "10px";
 
 			  
 			  	document.getElementById("sticky").append(plus); 
@@ -224,8 +162,6 @@ function addStickyNote(){
 			var heading = document.createElement("p");  
 				heading.style.fontFamily = "Roboto, Calibri!important";                     
 				heading.id = "testHead";
-				heading.style.margin="10%";
-				heading.style.marginBottom="5%";
 				//heading.innerText = "🌱 TASKS"; 
 				heading.innerText = "✨ TASKS"; 
 				//cell.innerHTML="Write your new task here!✨";           
@@ -233,21 +169,12 @@ function addStickyNote(){
 
 
 			//Create a new div element. This is the horizontal line below "GOAL" on the sticky note.
-			var line = document.createElement("div");    
-				line.style.width = "90%";
-				line.style.height = "1px";
-				line.style.margin = "auto";
-				line.style.background = "#D0D0D0";
+			var line = document.createElement("div");  
+				line.id = "grayLine";  
 				document.getElementById("sticky").append(line);                      
 
 			//Create a new div element. This is the area for appending goal statements. 
-			var goalDiv = document.createElement("div"); 
-				goalDiv.style.margin = "10%";
-				goalDiv.style.marginTop="5%";
-			    goalDiv.style.marginBottom = "50px";
-			    goalDiv.style.maxHeight="450px";
-			    goalDiv.style.fontFamily = "Roboto, Calibri!important";
-			    goalDiv.style.overflow="auto";                     
+			var goalDiv = document.createElement("div");       
 				goalDiv.id="goalDiv";
 
 				document.getElementById("sticky").append(goalDiv);   
